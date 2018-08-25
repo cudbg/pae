5344f06bd0f788b154c2d7c46957a1078dc41d8b
Gabriel Ryan <gabriel.ryan@columbia.edu>
2018-08-25 10:38:49 -0400

    pytests

---
+++ python/test/test_pae.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from pae import PAEMeasure
+
+
+def test_linear():
+    pae_meas = PAEMeasure(300, 200, 2, 20.0)
+
+    x = np.linspace(0,2,300)
+    linear_chart = x*0.5 - 0.5
+
+    saved_lin_res = np.asscalar(np.load('test/linear_result.npy'))
+
+    assert pae_meas.pae(linear_chart) == saved_lin_res
+
+    # print(lin_res)
+    # np.save('linear_result.npy', lin_res)
+
+
+
+def test_complex():
+    pae_meas = PAEMeasure(300, 200, 2, 20.0)
+
+    complex_chart = np.load('test/noised.npy')
+
+    saved_complex_res = np.asscalar(np.load('test/complex_result.npy'))
+    assert pae_meas.pae(complex_chart) == saved_complex_res
--- python/tests.py
+++
@@ -1,3 +0,0 @@
-from pae import PAE
-
-
