3e60feceac74a0cd475aeb6369f1577442271cf0
Gabriel Ryan <gabriel.ryan@columbia.edu>
2018-08-26 15:13:57 -0400

    add license and readme to setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 
 
-# with open('README.rst') as f:
-    # readme = f.read()
+with open('../README.md') as f:
+    readme = f.read()
 
-# with open('LICENSE') as f:
-    # license = f.read()
+with open('../LICENSE') as f:
+    license = f.read()
 
 setup(
     name='pae',
     version='1.0.0',
     description='Pixel Approximate Entropy',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gryan11/pae',
     author='Gabriel Ryan',
     author_email='gabriel.ryan@columbia.edu',
     install_requires=['numpy'],
